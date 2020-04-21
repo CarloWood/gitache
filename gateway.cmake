8f6bd6b50c9581bdd59228cca19e6c469a43e8b1
Carlo Wood <carlo@alinoe.com>
2020-04-22 00:36:07 +0200

    Write more DEBUG output.

--- gateway.cmake
+++ gateway.cmake
@@ -1,5 +1,8 @@
 cmake_minimum_required(VERSION 3.14)
 
+# To enable DEBUG messsages, pass -DCMAKE_MESSAGE_LOG_LEVEL=DEBUG to cmake on the command line.
+message(DEBUG "DEBUG: Entering ${CMAKE_CURRENT_LIST_FILE}")
+
 if (NOT DEFINED GITACHE_PACKAGES)
   message(FATAL_ERROR "GITACHE_PACKAGES must be set to a list of packages before including gitache/gateway.cmake.")
 endif ()
@@ -8,4 +11,6 @@ include(FetchContent)
 
 set(FETCHCONTENT_SOURCE_DIR_GITACHE "gitache" CACHE INTERNAL "" FORCE)  
 FetchContent_Declare(gitache)
+message(DEBUG "DEBUG: gitache/gateway.cmake: calling `FetchContent_MakeAvailable(gitache)`")
 FetchContent_MakeAvailable(gitache)  
+message(DEBUG "DEBUG: gitache/gateway.cmake: returned from `FetchContent_MakeAvailable(gitache)`")
